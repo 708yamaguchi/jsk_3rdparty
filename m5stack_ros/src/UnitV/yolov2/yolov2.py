20110f8c5ad8d72ef774c3f6f1d5b0ecc139ffa5
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-02-16 17:22:44 +0900

    Lower yolov2 detection threshold

--- m5stack_ros/src/UnitV/yolov2/yolov2.py
+++ m5stack_ros/src/UnitV/yolov2/yolov2.py
@@ -32,7 +32,7 @@ clock = time.clock()
 classes = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']  # NOQA
 task = kpu.load(0x500000)
 anchor = (1.08, 1.19, 3.42, 4.41, 6.63, 11.38, 9.42, 5.11, 16.62, 10.52)
-a = kpu.init_yolo2(task, 0.5, 0.3, 5, anchor)
+a = kpu.init_yolo2(task, 0.2, 0.3, 5, anchor)
 recog_flag = True
 
 # Packet header for UART, 4 byte
