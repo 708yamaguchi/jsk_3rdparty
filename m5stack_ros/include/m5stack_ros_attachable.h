c3bd6651047db37e2290aa6473392ae2a07281c2
Naoya Yamaguchi <708yamaguchi@gmail.com>
2022-04-07 17:41:39 +0900

    Display charging marker when charging

--- m5stack_ros/include/m5stack_ros_attachable.h
+++ m5stack_ros/include/m5stack_ros_attachable.h
@@ -56,6 +56,28 @@ void publishModuleInfo() {
   nh.spinOnce();
 }
 
+void blinkCharging() {
+  // 背景を真っ黒に塗りつぶす
+  M5.Lcd.fillScreen(BLACK);
+  // 電池へそ
+  M5.Lcd.fillRoundRect(280,45,25,110,5,WHITE);
+  // 大外枠
+  M5.Lcd.fillRoundRect(40,20,240,160,5,WHITE);
+  // 中枠
+  int battery_bar = (int) (220 * battery_level / 100.0);
+  M5.Lcd.fillRoundRect(50,30,battery_bar,140,5,GREEN);
+  // 充電マーク
+  M5.Lcd.fillTriangle(160, 65, 140, 110, 160, 110, BLACK);
+  M5.Lcd.fillTriangle(160, 135, 180, 90, 160, 90, BLACK);
+  // 点滅
+  delay(3000);
+  M5.Lcd.sleep();
+  M5.Lcd.setBrightness(0);
+  delay(3000);
+  M5.Lcd.wakeup();
+  M5.Lcd.setBrightness(255);
+}
+
 // This function is additional setup process for m5stack_ros.
 // this should be called after setup()
 void afterSetup() {
@@ -68,11 +90,16 @@ void afterSetup() {
 // This function should be called after every loop()
 void beforeLoop() {
   // Do not enter main loop when is_sleeping is true
-  while(is_sleeping){
+  while (is_sleeping || isCharging) {
+    if (is_sleeping) {
+      delay(1000);
+    }
+    if (isCharging) {
+      blinkCharging();
+    }
     nh.spinOnce();
     publishBattery();
     publishModuleInfo();
-    delay(1000);
   }
   publishBattery();
   publishModuleInfo();
